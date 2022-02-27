5e02ecc38c7e3d6e95331626b797e6e0a0c9c809
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2022-02-26 22:39:12 -0300

    Initial Commit

---
+++ MVVMCoordinator/MVVMCoordinatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMCoordinatorApp.swift
+//  MVVMCoordinator
+//
+//  Created by Luiz Araujo on 26/02/22.
+//
+
+import SwiftUI
+
+@main
+struct MVVMCoordinatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
